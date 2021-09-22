aff8cefe21a925cc2c539dc270e7c99744c4801e
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-09-22 00:00:54 -0400

    + Created an AIController class to override GetFocalPointOnActor(). + Removed FocusPoint on all character classes.

---
+++ Source/BluePrintTPS/MyAIController.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAIController.h"
+#include "MyCharacter.h"
+
+FVector AMyAIController::GetFocalPointOnActor(const AActor* Actor) const
+{
+	// Check if the actor is a character. If it does, get the half height of the capsule component.
+	if (Actor->IsA(AMyCharacter::StaticClass()))
+	{
+		return Actor->GetActorLocation() + FVector(0.0f, 0.0f, 88.0f);
+	}
+
+	// Otherwise, return the location of the character's pivot point.
+	return Actor->GetActorLocation();
+}
---
+++ Source/BluePrintTPS/MyAIController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLUEPRINTTPS_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	virtual FVector GetFocalPointOnActor(const AActor* Actor) const override;
+	
+};
